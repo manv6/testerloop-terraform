0966aacf345fa6454054323be858eb98ab3df631
Stefanos Varsanis <sbartsa@gmail.com>
2023-06-28 18:49:21 +0300

    Add docker build step

--- basic_lambda/main.tf
+++ basic_lambda/main.tf
@@ -1,188 +1,188 @@
-# variable "vpc_cidr_block" {}
-# variable "public_subnet_cidr_blocks" {}
-# variable "private_subnet_cidr_blocks" {}
-# variable "avail_zones" {}
-# variable "aws_region" {}
-# variable "npm_token" {}
+variable "vpc_cidr_block" {}
+variable "public_subnet_cidr_blocks" {}
+variable "private_subnet_cidr_blocks" {}
+variable "avail_zones" {}
+variable "aws_region" {}
+variable "npm_token" {}
 
-# module "vpc" {
-#   source = "terraform-aws-modules/vpc/aws"
-#   cidr   = var.vpc_cidr_block
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+  cidr   = var.vpc_cidr_block
 
-#   azs             = var.avail_zones
-#   private_subnets = var.private_subnet_cidr_blocks
-#   public_subnets  = var.public_subnet_cidr_blocks
+  azs             = var.avail_zones
+  private_subnets = var.private_subnet_cidr_blocks
+  public_subnets  = var.public_subnet_cidr_blocks
 
-#   enable_nat_gateway     = true
-#   enable_vpn_gateway     = false
-#   one_nat_gateway_per_az = true
+  enable_nat_gateway     = true
+  enable_vpn_gateway     = false
+  one_nat_gateway_per_az = true
 
-#   tags = {
-#     Terraform = "true"
-#     Name      = "testerloop-vpc"
-#   }
-# }
+  tags = {
+    Terraform = "true"
+    Name      = "testerloop-vpc"
+  }
+}
 
-# // Create the ecr repository
-# resource "aws_ecr_repository" "testerloop-lambda-ecr-repository" {
-#   name = "testerloop-lambda-ecr-repository"
-# }
+// Create the ecr repository
+resource "aws_ecr_repository" "testerloop-lambda-ecr-repository" {
+  name = "testerloop-lambda-ecr-repository"
+}
 
-# // Build and push the docker image to be used by the lambda
-# resource "null_resource" "docker_build_and_push" {
-#   depends_on = [aws_ecr_repository.testerloop-lambda-ecr-repository]
-#   provisioner "local-exec" {
-#     command = <<EOT
-#                  aws ecr get-login-password --region ${var.aws_region} | 
-#                  docker login --username AWS --password-stdin ${aws_ecr_repository.testerloop-lambda-ecr-repository.repository_url} && 
-#                  docker build --build-arg NPM_TOKEN=${var.npm_token} -t ${aws_ecr_repository.testerloop-lambda-ecr-repository.repository_url}:latest -f Dockerfile . &&
-#                  docker push ${aws_ecr_repository.testerloop-lambda-ecr-repository.repository_url}:latest 
-#                  EOT
-#   }
-# }
+// Build and push the docker image to be used by the lambda
+resource "null_resource" "docker_build_and_push" {
+  depends_on = [aws_ecr_repository.testerloop-lambda-ecr-repository]
+  provisioner "local-exec" {
+    command = <<EOT
+                 aws ecr get-login-password --region ${var.aws_region} | 
+                 docker login --username AWS --password-stdin ${aws_ecr_repository.testerloop-lambda-ecr-repository.repository_url} && 
+                 docker build --build-arg NPM_TOKEN=${var.npm_token} -t ${aws_ecr_repository.testerloop-lambda-ecr-repository.repository_url}:latest -f Dockerfile . &&
+                 docker push ${aws_ecr_repository.testerloop-lambda-ecr-repository.repository_url}:latest 
+                 EOT
+  }
+}
 
-# // Create the inbound and outbound security group rules
-# resource "aws_security_group_rule" "testerloop-security-group-rule-inbound-sg" {
-#   type                     = "ingress"
-#   security_group_id        = module.vpc.default_security_group_id
-#   description              = "Allow all inbound traffic from the security group"
-#   from_port                = 0
-#   to_port                  = 0
-#   protocol                 = "-1"
-#   source_security_group_id = module.vpc.default_security_group_id
-# }
+// Create the inbound and outbound security group rules
+resource "aws_security_group_rule" "testerloop-security-group-rule-inbound-sg" {
+  type                     = "ingress"
+  security_group_id        = module.vpc.default_security_group_id
+  description              = "Allow all inbound traffic from the security group"
+  from_port                = 0
+  to_port                  = 0
+  protocol                 = "-1"
+  source_security_group_id = module.vpc.default_security_group_id
+}
 
-# resource "aws_security_group_rule" "testerloop-security-group-rule-inbound-ipv4" {
-#   type              = "ingress"
-#   security_group_id = module.vpc.default_security_group_id
-#   description       = "Allow all inbound traffic"
-#   from_port         = 0
-#   to_port           = 0
-#   protocol          = "-1"
-#   cidr_blocks       = ["0.0.0.0/0"]
-# }
+resource "aws_security_group_rule" "testerloop-security-group-rule-inbound-ipv4" {
+  type              = "ingress"
+  security_group_id = module.vpc.default_security_group_id
+  description       = "Allow all inbound traffic"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
 
-# resource "aws_security_group_rule" "testerloop-security-group-rule-outbound-sg" {
-#   type                     = "egress"
-#   security_group_id        = module.vpc.default_security_group_id
-#   description              = "Allow all outbound traffic from the security group"
-#   from_port                = 0
-#   to_port                  = 0
-#   protocol                 = "-1"
-#   source_security_group_id = module.vpc.default_security_group_id
-# }
+resource "aws_security_group_rule" "testerloop-security-group-rule-outbound-sg" {
+  type                     = "egress"
+  security_group_id        = module.vpc.default_security_group_id
+  description              = "Allow all outbound traffic from the security group"
+  from_port                = 0
+  to_port                  = 0
+  protocol                 = "-1"
+  source_security_group_id = module.vpc.default_security_group_id
+}
 
-# resource "aws_security_group_rule" "testerloop-security-group-rule-outbound-ipv4" {
-#   type              = "egress"
-#   security_group_id = module.vpc.default_security_group_id
-#   description       = "Allow all outbound traffic"
-#   from_port         = 0
-#   to_port           = 0
-#   protocol          = "-1"
-#   cidr_blocks       = ["0.0.0.0/0"]
-# }
-
-
-# // Create the s3 bucket
-# resource "aws_s3_bucket" "testerloopS3Bucket" {
-#   bucket = "testerloop-tests-results-test-hd1"
-# }
+resource "aws_security_group_rule" "testerloop-security-group-rule-outbound-ipv4" {
+  type              = "egress"
+  security_group_id = module.vpc.default_security_group_id
+  description       = "Allow all outbound traffic"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
 
 
+// Create the s3 bucket
+resource "aws_s3_bucket" "testerloopS3Bucket" {
+  bucket = "testerloop-tests-results-test-hd1"
+}
 
-# // Create the service role for the lambda
-# resource "aws_iam_role" "testerloopLambdaCypressRole" {
-#   path                 = "/service-role/"
-#   name                 = "testerloop-cypress-lambda-role"
-#   assume_role_policy   = "{\"Version\":\"2012-10-17\",\"Statement\":[{\"Effect\":\"Allow\",\"Principal\":{\"Service\":\"lambda.amazonaws.com\"},\"Action\":\"sts:AssumeRole\"}]}"
-#   max_session_duration = 3600
-#   tags                 = {}
-# }
 
-# // Create the policy to write to s3
-# resource "aws_iam_role_policy" "testerloopWriteResultsToS3Policy" {
-#   name   = "TesterloopS3WriteResultsPolicy"
-#   policy = <<EOF
-# {
-#     "Version": "2012-10-17",
-#     "Statement": [
-#         {
-#             "Effect": "Allow",
-#             "Action": [
-#                 "s3:PutObject",
-#                 "s3:GetObject",
-#                 "s3:AbortMultipartUpload",
-#                 "s3:ListBucket",
-#                 "s3:GetObjectVersion",
-#                 "s3:ListMultipartUploadParts"
-#             ],
-#             "Resource": [
-#                 "${aws_s3_bucket.testerloopS3Bucket.arn}/*",
-#                 "${aws_s3_bucket.testerloopS3Bucket.arn}"
-#             ]
-#         }
-#     ]
-# }
-# EOF
-#   role   = aws_iam_role.testerloopLambdaCypressRole.name
-# }
 
-# // Create the policy to allow the lambda to attach to the VPC
-# resource "aws_iam_role_policy" "testerloopEnableLambdaToAttachVpcPolicy" {
-#   name   = "LambdaVPCAccessExecutionPolicy"
-#   policy = <<EOF
-# {
-#     "Version": "2012-10-17",
-#     "Statement": [
-#         {
-#             "Effect": "Allow",
-#             "Action": [
-#                 "logs:CreateLogGroup",
-#                 "logs:CreateLogStream",
-#                 "logs:PutLogEvents",
-#                 "ec2:AttachNetworkInterface",
-#                 "ec2:DescribeDhcpOptions",
-#                 "ec2:DescribeInstances",
-#                 "ec2:DescribeRouteTables",
-#                 "ec2:CreateNetworkInterface",
-#                 "ec2:DescribeNetworkInterfaces",
-#                 "ec2:DeleteNetworkInterface",
-#                 "ec2:AssignPrivateIpAddresses",
-#                 "ec2:UnassignPrivateIpAddresses"
-#             ],
-#             "Resource": "*"
+// Create the service role for the lambda
+resource "aws_iam_role" "testerloopLambdaCypressRole" {
+  path                 = "/service-role/"
+  name                 = "testerloop-cypress-lambda-role"
+  assume_role_policy   = "{\"Version\":\"2012-10-17\",\"Statement\":[{\"Effect\":\"Allow\",\"Principal\":{\"Service\":\"lambda.amazonaws.com\"},\"Action\":\"sts:AssumeRole\"}]}"
+  max_session_duration = 3600
+  tags                 = {}
+}
 
-#         }
-#     ]
-# }
-# EOF
-#   role   = aws_iam_role.testerloopLambdaCypressRole.name
-# }
+// Create the policy to write to s3
+resource "aws_iam_role_policy" "testerloopWriteResultsToS3Policy" {
+  name   = "TesterloopS3WriteResultsPolicy"
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:PutObject",
+                "s3:GetObject",
+                "s3:AbortMultipartUpload",
+                "s3:ListBucket",
+                "s3:GetObjectVersion",
+                "s3:ListMultipartUploadParts"
+            ],
+            "Resource": [
+                "${aws_s3_bucket.testerloopS3Bucket.arn}/*",
+                "${aws_s3_bucket.testerloopS3Bucket.arn}"
+            ]
+        }
+    ]
+}
+EOF
+  role   = aws_iam_role.testerloopLambdaCypressRole.name
+}
 
-# // Create the lalmbda function
-# resource "aws_lambda_function" "testerloopLambdaFunction" {
-#   depends_on  = [null_resource.docker_build_and_push]
-#   description = "Lambda function for executing the Testerloop tests"
+// Create the policy to allow the lambda to attach to the VPC
+resource "aws_iam_role_policy" "testerloopEnableLambdaToAttachVpcPolicy" {
+  name   = "LambdaVPCAccessExecutionPolicy"
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "logs:CreateLogGroup",
+                "logs:CreateLogStream",
+                "logs:PutLogEvents",
+                "ec2:AttachNetworkInterface",
+                "ec2:DescribeDhcpOptions",
+                "ec2:DescribeInstances",
+                "ec2:DescribeRouteTables",
+                "ec2:CreateNetworkInterface",
+                "ec2:DescribeNetworkInterfaces",
+                "ec2:DeleteNetworkInterface",
+                "ec2:AssignPrivateIpAddresses",
+                "ec2:UnassignPrivateIpAddresses"
+            ],
+            "Resource": "*"
 
-#   function_name = "testerloop-cypress-lambda"
-#   architectures = [
-#     "x86_64"
-#   ]
-#   package_type = "Image"
-#   image_uri    = "${aws_ecr_repository.testerloop-lambda-ecr-repository.repository_url}:latest"
-#   memory_size  = 4096
-#   role         = aws_iam_role.testerloopLambdaCypressRole.arn
-#   timeout      = 600
+        }
+    ]
+}
+EOF
+  role   = aws_iam_role.testerloopLambdaCypressRole.name
+}
 
-#   tracing_config {
-#     mode = "PassThrough"
-#   }
-#   vpc_config {
-#     subnet_ids = [module.vpc.public_subnets[0], module.vpc.private_subnets[0]]
-#     security_group_ids = [
-#       module.vpc.default_security_group_id
-#     ]
-#   }
-# }
+// Create the lalmbda function
+resource "aws_lambda_function" "testerloopLambdaFunction" {
+  depends_on  = [null_resource.docker_build_and_push]
+  description = "Lambda function for executing the Testerloop tests"
+
+  function_name = "testerloop-cypress-lambda"
+  architectures = [
+    "x86_64"
+  ]
+  package_type = "Image"
+  image_uri    = "${aws_ecr_repository.testerloop-lambda-ecr-repository.repository_url}:latest"
+  memory_size  = 4096
+  role         = aws_iam_role.testerloopLambdaCypressRole.arn
+  timeout      = 600
+
+  tracing_config {
+    mode = "PassThrough"
+  }
+  vpc_config {
+    subnet_ids = [module.vpc.public_subnets[0], module.vpc.private_subnets[0]]
+    security_group_ids = [
+      module.vpc.default_security_group_id
+    ]
+  }
+}
 
 
--- basic_lambda/terraform.tfvars
+++ basic_lambda/terraform.tfvars
@@ -4,3 +4,4 @@ private_subnet_cidr_blocks = ["10.0.1.0/24"]
 avail_zones                = ["eu-west-3a"]
 aws_region                 = "eu-west-3"
 npm_token                  = ""
+
